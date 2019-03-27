6ab70fda79b991363527981d9562679ab660ac97
wangchun@gmail.com <Tt20MWC0ugehH2hv>
2019-03-27 14:43:17 +0800

    1.00

--- src/api/JsonResponseFormatter.php
+++ src/api/JsonResponseFormatter.php
@@ -3,33 +3,31 @@
 namespace springchun\yii2\core\api;
 
 use Exception;
-use function var_dump;
 use yii\rest\Serializer;
-use yii\web\JsonResponseFormatter;
 
 /**
  * Class ApiFormatter
  * @package springchun\yii2\core\api
  */
-class ApiResponseFormatter extends JsonResponseFormatter
+class JsonResponseFormatter extends \yii\web\JsonResponseFormatter
 {
     /**
      * @param \yii\web\Response $response
      */
     protected function formatJson($response)
     {
-        if(\Yii::$app->errorHandler->exception instanceof Exception){
+        if (\Yii::$app->errorHandler->exception instanceof Exception) {
             $response->data = \Yii::$app->errorHandler->exception;
         }
         if ($response->data) {
             if ($response->data instanceof Exception) {
                 $response->setStatusCodeByException($response->data);
                 $response->data = [
-                    'code'=>$response->data->getCode(),
-                    'message'=>$response->data->getMessage(),
-                    'data'=>null
+                    'code' => $response->data->getCode(),
+                    'message' => $response->data->getMessage(),
+                    'data' => null
                 ];
-            }else{
+            } else {
                 $data = (new Serializer())->serialize($response->data);
                 if ($response->isSuccessful) {
                     $response->data = [
@@ -41,7 +39,7 @@ class ApiResponseFormatter extends JsonResponseFormatter
                     $response->data = [
                         'code' => 1,
                         'data' => null,
-                        'message'=>$response->statusText
+                        'message' => $response->statusText
                     ];
                 }
             }
